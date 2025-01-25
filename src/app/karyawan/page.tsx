caf70c58dbaa6b29da09d7cfbd66da32d199ce44
furqonstudio <furqonstudio@gmail.com>
2025-01-25 16:10:26 +0700

    feat: add karyawan-table

--- src/app/karyawan/page.tsx
+++ src/app/karyawan/page.tsx
@@ -1,10 +1,12 @@
 import { Container } from '@/components/container'
+import { KaryawanTable } from '@/components/karyawan-table'
 import React from 'react'
 
 const KaryawanPage = () => {
   return (
     <Container>
       <h2 className="font-bold text-xl">Daftar Karyawan</h2>
+      <KaryawanTable />
     </Container>
   )
 }
