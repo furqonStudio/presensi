771c773b8278abd039d8d9b1b2746a8cf3693ebf
furqonstudio <furqonstudio@gmail.com>
2025-01-30 17:57:57 +0700

    fea: add radius location

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,9 +5,9 @@ import {
   Marker,
   Popup,
   useMapEvents,
+  Circle, // Impor Circle untuk menggambar radius
 } from 'react-leaflet'
 import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
 
 export function Map({
   initialCoordinates = [-6.2088, 106.8456], // Default ke Jakarta
@@ -39,18 +39,28 @@ export function Map({
     })
 
     return position ? (
-      <Marker position={position} icon={customIcon}>
-        <Popup>
-          Koordinat: {position[0]}, {position[1]}
-        </Popup>
-      </Marker>
+      <>
+        <Marker position={position} icon={customIcon}>
+          <Popup>
+            Koordinat: {position[0]}, {position[1]}
+          </Popup>
+        </Marker>
+
+        <Circle
+          center={position} // Pusat lingkaran di posisi marker
+          radius={30} // Radius dalam meter
+          color="blue" // Warna garis lingkaran
+          fillColor="blue" // Warna isi lingkaran
+          fillOpacity={0.3} // Opasitas warna isi lingkaran
+        />
+      </>
     ) : null
   }
 
   return (
     <MapContainer
       center={position}
-      zoom={13}
+      zoom={20}
       style={{ height: '300px', width: '100%' }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
