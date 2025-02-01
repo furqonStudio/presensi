ff82b37b5dd9a413a9bf17764d3215bda1a921b0
furqonstudio <furqonstudio@gmail.com>
2025-02-01 18:37:41 +0700

    feat: add middleware protected routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { LoginForm } from '@/components/login-form'
 import { RegisterForm } from '@/components/register-form'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const [isRegister, setIsRegister] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    // Ganti dengan pengecekan login yang sesuai
+    const isLoggedIn = Boolean(localStorage.getItem('authToken')) // atau sesuaikan dengan pengecekan status login Anda
+
+    if (isLoggedIn) {
+      router.push('/dashboard')
+    } else {
+      router.push('/')
+    }
+  }, [router])
 
   return (
     <div className="flex h-full w-full items-center justify-center bg-gradient-to-tr from-green-950 to-blue-950 p-4">
---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  // const authToken = request.cookies.get('authToken')
+  const authToken = true
+
+  if (authToken && request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
+  const protectedPaths = ['/dashboard', '/kantor', '/shift', '/karyawan']
+
+  if (
+    protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))
+  ) {
+    if (!authToken) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/dashboard', '/kantor', '/shift', '/karyawan', '/'],
+}
